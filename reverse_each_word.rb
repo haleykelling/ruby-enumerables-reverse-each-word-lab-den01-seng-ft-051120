51c67eb38a08aa6ba6ab149f49f716605a9df56f
Haley Kelling <haley@kellings.com>
2020-04-22 15:43:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split
   array.collect do |word|
-    word.reverse 
+    word.reverse
+  end
 end
 
 reverse_each_word("hello everyone my name is haley")
