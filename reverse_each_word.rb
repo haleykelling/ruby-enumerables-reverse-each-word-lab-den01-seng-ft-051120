79991695c2a7c91134afe2c4a525764b2ddfc15d
Haley Kelling <haley@kellings.com>
2020-04-22 15:47:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split
-  array.collect do |word|
+  reverse_array = array.collect do |word|
     word.reverse
   end
-  array.join(" ")
+  reverse_array.join(" ")
 end
